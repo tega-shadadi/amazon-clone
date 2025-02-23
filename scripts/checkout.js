a63e3f679a4dff391dc2ebf132a3b573a4af31e5
Shadadi <brynkeener12@gmail.com>
2025-02-24 00:01:06 +0300

    fix currency format and radio selectors in checkout section

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js"
 
 // Array to store selected products with their quantities
 const cartItems = [];
@@ -52,7 +53,7 @@ function renderCartItems() {
                   ${item.name}
                 </div>
                 <div class="product-price">
-                  $${(item.priceCents/100).toFixed(2)}
+                  $${formatCurrency(item.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -74,7 +75,7 @@ function renderCartItems() {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${item.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -87,7 +88,7 @@ function renderCartItems() {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${item.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -100,7 +101,7 @@ function renderCartItems() {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${item.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
